bc9b950a3f52f6c84390b34e2124428403016f2c
pradeepmajji853 <pradeepmajji853@gmail.com>
2025-02-06 19:28:50 +0530

    authenication:

--- FinanceTracker/src/components/App.jsx
+++ FinanceTracker/src/components/App.jsx
@@ -1,17 +1,13 @@
-import "./App.css";
-import Heropage from "./Heropage.jsx";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Features from "./Features.jsx";
-import Getstarted from "./Getstarted.jsx";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { ClerkProvider, SignIn, SignUp, SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
 import Layout from "./Layout.jsx";
 import Dashboard from "./Dashboard.jsx";
-import Overview from "./Overview.jsx";
-import Budgets from "./Budgets.jsx";
-import SavingsWallet from "./SavingsWallet.jsx";
-import BankAccountdashboard from "./BankAccountdashboard.jsx";
-import { SignIn, SignUp, SignedIn, SignedOut, RedirectToSignIn } from "@clerk/clerk-react";
+import "./App.css";
 
 function App() {
+  const location = useLocation();
+
   return (
     <Router>
       <Routes>
@@ -32,7 +28,7 @@ function App() {
           path="/login"
           element={
             <Layout>
-              <SignIn afterSignInUrl="/dashboard" />
+              <SignIn afterSignInUrl={new URLSearchParams(location.search).get('after_sign_in_url') || '/dashboard'} />
             </Layout>
           }
         />
@@ -40,7 +36,7 @@ function App() {
           path="/register"
           element={
             <Layout>
-              <SignUp afterSignUpUrl="/dashboard" />
+              <SignUp afterSignUpUrl={new URLSearchParams(location.search).get('after_sign_up_url') || '/dashboard'} />
             </Layout>
           }
         />
@@ -56,46 +52,6 @@ function App() {
             </SignedIn>
           }
         />
-        <Route
-          path="/overview"
-          element={
-            <SignedIn>
-              <Layout>
-                <Overview />
-              </Layout>
-            </SignedIn>
-          }
-        />
-        <Route
-          path="/budgets"
-          element={
-            <SignedIn>
-              <Layout>
-                <Budgets />
-              </Layout>
-            </SignedIn>
-          }
-        />
-        <Route
-          path="/savingswallet"
-          element={
-            <SignedIn>
-              <Layout>
-                <SavingsWallet />
-              </Layout>
-            </SignedIn>
-          }
-        />
-        <Route
-          path="/bankaccountdashboard"
-          element={
-            <SignedIn>
-              <Layout>
-                <BankAccountdashboard />
-              </Layout>
-            </SignedIn>
-          }
-        />
 
         {/* Catch-all Route for SignedOut Users */}
         <Route
@@ -105,12 +61,6 @@ function App() {
               <RedirectToSignIn />
             </SignedOut>
           }
-        />
-
-        {/* Fallback Route for Unauthorized Access */}
-        <Route
-          path="/unauthorized"
-          element={<Navigate to="/login" />}
         />
       </Routes>
     </Router>
